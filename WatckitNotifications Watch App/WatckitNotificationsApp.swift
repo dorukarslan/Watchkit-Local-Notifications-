4141f70f526e139f6211a734d12b9f5111fbc12e
dorukarslan <dorukasdfgh@gmail.com>
2022-12-11 13:51:02 +0300

    Initial Commit

---
+++ WatckitNotifications Watch App/WatckitNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatckitNotificationsApp.swift
+//  WatckitNotifications Watch App
+//
+//  Created by Doruk Arslan on 11.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct WatckitNotifications_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
